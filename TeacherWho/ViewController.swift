aee87a90056afd69e1ce3da6223aaaaff756010f
Idan <idan8ezer@gmail.com>
2022-06-03 11:17:19 +0300

    Initial Commit

---
+++ TeacherWho/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeacherWho
+//
+//  Created by Liron Ezer on 03/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
